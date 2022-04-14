846df84c8dbb443519d5d01ee4437c60641e6ae6
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-04-14 17:00:09 +0530

    CREATE: current user accessed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_notes/firebase_options.dart';
 
 import 'package:my_notes/views/login_view.dart';
 
@@ -9,7 +12,39 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const LoginView(),
+    home: const HomeView(),
     debugShowCheckedModeBanner: false,
   ));
+}
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            final user = FirebaseAuth.instance.currentUser;
+
+            if (user?.emailVerified ?? false) {
+              print('Email verified');
+            } else {
+              print('Email not verified');
+            }
+            return snapshot.connectionState == ConnectionState.done
+                ? const Text('Done')
+                : const Text('Loading...');
+          }),
+    );
+  }
 }
